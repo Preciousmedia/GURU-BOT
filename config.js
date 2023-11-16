ffac44d9148d8061439e9fd0d3673b9ed02c9ab5
Preciousmedia <144480169+Preciousmedia@users.noreply.github.com>
2023-11-16 23:54:15 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['2349079835603', 'Great media', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
